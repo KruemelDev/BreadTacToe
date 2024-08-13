4d65e6aa5cbfc771efb3be64afdc991a8aba52b5
KruemelDev <lennartsigge@icloud.com>
2024-08-13 22:16:31 +0200

    Get game fully working

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ class Player:
     def __init__(self, id, sign, winning_board):
         self.id = id
         self.sign = sign
-        self.placedPos = ["", "", "", "", "", "", "", "", ""]
-        self.winningBoard = ["", "", "", "", "", "", "", "", "", ""]
+        self.placed_pos = ["", "", "", "", "", "", "", "", ""]
+        self.winning_board = ["", "", "", "", "", "", "", "", "", ""]
 
     def place(self, inputManager):
-        inputManager.place(self.placedPos)
+        inputManager.place(self.placed_pos)
 
 
 class GameManager:
@@ -23,7 +23,7 @@ class GameManager:
         self.board = ["", "", "", "", "", "", "", "", ""]
         self.player1 = player1
         self.player2 = player2
-        self.currentPlayer = player1
+        self.current_player = player1
         self.winner = None
         self.winning = False
 
@@ -42,36 +42,36 @@ class GameManager:
             if self.possible_to_place():
                 input_manager.input_handling()
 
-        self.draw_board(self.currentPlayer.winningBoard, self.currentPlayer.sign)
-
+        self.draw_board(self.winner.winning_board, self.winner.sign)
+        time.sleep(5.0)
     def possible_to_place(self):
+        space_to_place = False
         for signPosition in self.board:
             if signPosition == "":
-                return True
-            else:
-                return False
+                space_to_place = True
+        return space_to_place
 
     def next_player(self):
-        if self.currentPlayer.id == 1:
-            self.currentPlayer = self.player2
+        if self.current_player.id == 1:
+            self.current_player = self.player2
         else:
-            self.currentPlayer = self.player1
+            self.current_player = self.player1
 
     def get_current_player_id(self):
-        return self.currentPlayer.id
+        return self.current_player.id
 
     def place_sign_and_next_player(self, pos):
         if self.allowed_to_place_pos(pos):
-            self.board[pos] = self.currentPlayer.sign
-            self.currentPlayer.placedPos[pos] = self.currentPlayer.sign
-            print("last player was" + str(self.currentPlayer.id))
+            self.board[pos] = self.current_player.sign
+            self.current_player.placed_pos[pos] = self.current_player.sign
+            print("last player was" + str(self.current_player.id))
             print("places at " + str(pos))
-            self.draw_board(self.currentPlayer.placedPos, self.currentPlayer.sign)
+            self.draw_board(self.current_player.placed_pos, self.current_player.sign)
             if self.check_for_win():
                 self.winning = True
-                self.winner = self.currentPlayer.id
+                self.winner = self.current_player
             self.next_player()
-
+            self.draw_board(self.current_player.placed_pos, self.current_player.sign)
             return True
         else:
             return False
@@ -83,21 +83,21 @@ class GameManager:
             return False
 
     def check_for_win(self):
-        if self.board[0] == self.currentPlayer.sign and self.board[1] == self.currentPlayer.sign and self.board[2] == self.currentPlayer.sign:
+        if self.board[0] == self.current_player.sign and self.board[1] == self.current_player.sign and self.board[2] == self.current_player.sign:
             return True
-        elif self.board[3] == self.currentPlayer.sign and self.board[4] == self.currentPlayer.sign and self.board[5] == self.currentPlayer.sign:
+        elif self.board[3] == self.current_player.sign and self.board[4] == self.current_player.sign and self.board[5] == self.current_player.sign:
             return True
-        elif self.board[6] == self.currentPlayer.sign and self.board[7] == self.currentPlayer.sign and self.board[8] == self.currentPlayer.sign:
+        elif self.board[6] == self.current_player.sign and self.board[7] == self.current_player.sign and self.board[8] == self.current_player.sign:
             return True
-        elif self.board[0] == self.currentPlayer.sign and self.board[3] == self.currentPlayer.sign and self.board[6] == self.currentPlayer.sign:
+        elif self.board[0] == self.current_player.sign and self.board[3] == self.current_player.sign and self.board[6] == self.current_player.sign:
             return True
-        elif self.board[1] == self.currentPlayer.sign and self.board[4] == self.currentPlayer.sign and self.board[7] == self.currentPlayer.sign:
+        elif self.board[1] == self.current_player.sign and self.board[4] == self.current_player.sign and self.board[7] == self.current_player.sign:
             return True
-        elif self.board[2] == self.currentPlayer.sign and self.board[5] == self.currentPlayer.sign and self.board[8] == self.currentPlayer.sign:
+        elif self.board[2] == self.current_player.sign and self.board[5] == self.current_player.sign and self.board[8] == self.current_player.sign:
             return True
-        elif self.board[0] == self.currentPlayer.sign and self.board[4] == self.currentPlayer.sign and self.board[8] == self.currentPlayer.sign:
+        elif self.board[0] == self.current_player.sign and self.board[4] == self.current_player.sign and self.board[8] == self.current_player.sign:
             return True
-        elif self.board[2] == self.currentPlayer.sign and self.board[4] == self.currentPlayer.sign and self.board[6] == self.currentPlayer.sign:
+        elif self.board[2] == self.current_player.sign and self.board[4] == self.current_player.sign and self.board[6] == self.current_player.sign:
             return True
         else:
             return False
@@ -105,7 +105,7 @@ class GameManager:
     @staticmethod
     def draw_board(board, sign):
 
-        for i in range(len(board)):
+        for i in range(9):
             if board[i] == sign:
                 GPIO.setup(gpio_list_leds[i], GPIO.OUT)
                 GPIO.output(gpio_list_leds[i], GPIO.HIGH)
@@ -131,12 +131,12 @@ class InputManager:
     def right_button(self):
         if GPIO.input(self.buttonRightPin) == 1:
             if self.board_to_draw == 1:
-                GameManager.draw_board(player2.placedPos, player2.sign)
+                GameManager.draw_board(player2.placed_pos, player2.sign)
                 self.board_to_draw = 2
                 time.sleep(0.15)
                 print("draw_board 1")
             elif self.board_to_draw == 2:
-                GameManager.draw_board(player1.placedPos, player1.sign)
+                GameManager.draw_board(player1.placed_pos, player1.sign)
                 self.board_to_draw = 1
                 print("draw_board 2")
                 time.sleep(0.15)
@@ -152,14 +152,16 @@ class InputManager:
             else:
                 if not self.current_place_pos == 8:
                     self.current_place_pos += 1
+                    print("current place pos" + str(self.current_place_pos))
                 else:
                     self.current_place_pos = 0
+                    print("current place pos" + str(self.current_place_pos))
 
             time.sleep(0.2)
 
 
 if __name__ == "__main__":
-    player1 = Player(1, "X", ["", "X", "", "X", "X", "", "", "", "X"])
+    player1 = Player(1, "X", ["", "X", "", "X", "X", "", "", "X", ""])
     player2 = Player(2, "0", ["", "0", "0", "0", "0", "", "0", "0", "0"])
     gameManager = GameManager(player1, player2)
     gameManager.start_game()
